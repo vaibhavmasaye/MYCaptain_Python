bfb9adfc7f95c00879cab47675562a91b485a7af
vaibhavmasaye <47131806+vaibhavmasaye@users.noreply.github.com>
2021-12-01 09:44:25 +0530

    Update Data_Structures.py

--- Data Structures/Data_Structures.py
+++ Data Structures/Data_Structures.py
@@ -1,5 +1,7 @@
-Data Structures
-
+# Data Structures
+# Python program which accepts the radius of a circle from the user and computes area.Python program to print Positive Numbers in a List
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius  " + str(r) + " is: " + str(pi * r**2))
+
+# Python program to accept a filename from the user and print the extension of that.
