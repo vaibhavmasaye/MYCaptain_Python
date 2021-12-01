dda5d627e974f767469370001ee39b6bafa46ccb
vaibhavmasaye <47131806+vaibhavmasaye@users.noreply.github.com>
2021-12-01 15:46:49 +0530

    Update 1s_task.py

--- Data Structures/1s_task.py
+++ Data Structures/1s_task.py
@@ -1,3 +1,3 @@
-from math import pi
-r = float(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius  " + str(r) + " is: " + str(pi * r**2))
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
