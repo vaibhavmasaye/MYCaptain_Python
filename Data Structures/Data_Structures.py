5a3eae28f3277fc861ed76979323a9de69062761
vaibhavmasaye <47131806+vaibhavmasaye@users.noreply.github.com>
2021-12-01 09:45:05 +0530

    Update Data_Structures.py

--- Data Structures/Data_Structures.py
+++ Data Structures/Data_Structures.py
@@ -5,3 +5,7 @@ r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius  " + str(r) + " is: " + str(pi * r**2))
 
 # Python program to accept a filename from the user and print the extension of that.
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
